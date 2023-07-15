faf024e8f82276b6313a955ef773e378a5655a9f
NaveenSharma <you@example.comns222280@gmail.com>
2023-07-15 13:27:09 +0530

    application/json api demo

--- webServer.js
+++ webServer.js
@@ -1,15 +1,19 @@
 const http = require("http");
+const fs = require("fs");
 
-const data = {
-    age:5
-}
+// const indexhtml = fs.readFileSync("index.html","utf-8")
+const data = fs.readFileSync("data.json","utf-8")
+
+
+// =====================server=====================
 
 const server = http.createServer((req, res)=>{
     console.log("server started")
     res.setHeader('dummy','dummyValue')
-    res.setHeader('content-type', 'text/html')
+    // res.setHeader('content-type', 'text/html')
+    res.setHeader('content-type', 'application/json')
     // res.end(JSON.stringify(data));
-    res.end("hello world")
+    res.end(data)
     console.log(req.url)
 })
 server.listen(3000)
