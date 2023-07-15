bd662af8761cb10a4597004aee43f95a6f5c589c
NaveenSharma <you@example.comns222280@gmail.com>
2023-07-15 12:59:58 +0530

    headers completed

--- webServer.js
+++ webServer.js
@@ -1,7 +1,15 @@
 const http = require("http");
 
+const data = {
+    age:5
+}
+
 const server = http.createServer((req, res)=>{
     console.log("server started")
-    res.end("<h1>hello World</h1>")
+    res.setHeader('dummy','dummyValue')
+    res.setHeader('content-type', 'text/html')
+    // res.end(JSON.stringify(data));
+    res.end("hello world")
+    console.log(req.url)
 })
 server.listen(3000)
