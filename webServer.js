925f535e78ebee6ec5987b4fe6b3c0ad3d2918d2
NaveenSharma <you@example.comns222280@gmail.com>
2023-07-15 12:46:49 +0530

    print hello world in node js

--- webServer.js
+++ webServer.js
@@ -1,1 +1,7 @@
-console.log("naveen");
+const http = require("http");
+
+const server = http.createServer((req, res)=>{
+    console.log("server started")
+    res.end("<h1>hello World</h1>")
+})
+server.listen(3000)
