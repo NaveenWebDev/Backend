a661c1eec51864cf3e37bfe80067f47df7254f2e
NaveenWebDev <ns222280@gmail.com>
2023-07-20 00:30:54 -0700

    basic api with express

--- index.js
+++ index.js
@@ -3,15 +3,36 @@ const fs = require("fs")
 const server = express();
 const data = JSON.parse(fs.readFileSync("data.json","utf-8"));
 
-server.get('/', (req, res)=>{
-    // res.send("<h1>Hello world</h1>");
-    // res.sendFile('C:/Users/Naveen/Desktop/New folder/Backend/index.html')
-    // res.json(data)
-    res.sendStatus(201).send("<h1>hello</h1>");
-
-    // console.log(data);
-
+server.get('/', (req,res)=>{
+    res.json({type:"GET"})
 })
+server.post('/', (req,res)=>{
+    res.json({type:"POST"})
+})
+server.put('/', (req,res)=>{
+    res.json({type:"PUT"})
+})
+server.delete('/', (req,res)=>{
+    res.json({type:"DELETE"})
+})
+server.patch('/', (req,res)=>{
+    res.json({type:"PATCH"})
+})
+
+
+
+
+
+
+// server.get('/', (req, res)=>{
+//     // res.send("<h1>Hello world</h1>");
+//     // res.sendFile('C:/Users/Naveen/Desktop/New folder/Backend/index.html')
+//     // res.json(data)
+//     res.sendStatus(201).send("<h1>hello</h1>");
+
+//     // console.log(data);
+
+// })
 
 
 
