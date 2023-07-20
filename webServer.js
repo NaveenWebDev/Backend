40709cd9c5e4c83d0009f191b08c4e5b24c9fc35
NaveenWebDev <ns222280@gmail.com>
2023-07-19 23:10:02 -0700

    server with swithch case

--- webServer.js
+++ webServer.js
@@ -1,19 +1,28 @@
 const http = require("http");
 const fs = require("fs");
 
-// const indexhtml = fs.readFileSync("index.html","utf-8")
+const indexhtml = fs.readFileSync("index.html","utf-8")
 const data = fs.readFileSync("data.json","utf-8")
 
 
 // =====================server=====================
 
 const server = http.createServer((req, res)=>{
-    console.log("server started")
-    res.setHeader('dummy','dummyValue')
-    // res.setHeader('content-type', 'text/html')
-    res.setHeader('content-type', 'application/json')
-    // res.end(JSON.stringify(data));
-    res.end(data)
-    console.log(req.url)
+
+    switch (req.url) {
+        case '/':
+            res.setHeader('content-type', 'text/html')
+            res.end(indexhtml)
+            break;
+        case '/api':
+            res.setHeader('content-type', 'application/json')
+            // res.end(JSON.stringify(data));
+            res.end(data)
+            break;
+        default:
+            break;
+    }
+    // res.setHeader('dummy','dummyValue')
+    // console.log(req.url)
 })
 server.listen(3000)
