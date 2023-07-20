3a0a314a0d62674493884f0243c984199357d484
NaveenWebDev <ns222280@gmail.com>
2023-07-19 23:40:24 -0700

    node completed

--- webServer.js
+++ webServer.js
@@ -12,7 +12,8 @@ const server = http.createServer((req, res)=>{
     switch (req.url) {
         case '/':
             res.setHeader('content-type', 'text/html')
-            res.end(indexhtml)
+            const ind = indexhtml.replace('**Love**', "Laptop").replace("*Title*", "I am a Hacker")
+            res.end(ind)
             break;
         case '/api':
             res.setHeader('content-type', 'application/json')
