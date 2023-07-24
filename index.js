39361f9b70bd158d1ccfc2de60bf7d516c2616f3
NaveenWebDev <ns222280@gmail.com>
2023-07-23 23:33:49 -0700

    middle ware

--- index.js
+++ index.js
@@ -3,10 +3,30 @@ const fs = require("fs")
 const server = express();
 const data = JSON.parse(fs.readFileSync("data.json","utf-8"));
 
-server.get('/', (req,res)=>{
+// const auth = (req, res, next)=>{
+//     // console.log(req.method)
+//     if(req.query.password == '123'){
+//         next()
+//     }else{
+//         res.sendStatus(401)
+//     }
+// }
+// // server.use(auth)
+const auth = (req, res, next)=>{
+    // console.log(req.method)
+    if(req.body.password == '123'){
+        next()
+    }else{
+        res.sendStatus(401)
+    }
+}
+// server.use(auth)
+
+
+server.get('/', auth, (req,res)=>{
     res.json({type:"GET"})
 })
-server.post('/', (req,res)=>{
+server.post('/', auth, (req,res)=>{
     res.json({type:"POST"})
 })
 server.put('/', (req,res)=>{
@@ -19,23 +39,13 @@ server.patch('/', (req,res)=>{
     res.json({type:"PATCH"})
 })
 
-
-
-
-
-
 // server.get('/', (req, res)=>{
 //     // res.send("<h1>Hello world</h1>");
 //     // res.sendFile('C:/Users/Naveen/Desktop/New folder/Backend/index.html')
 //     // res.json(data)
 //     res.sendStatus(201).send("<h1>hello</h1>");
-
 //     // console.log(data);
-
 // })
-
-
-
 
 server.listen(4000, ()=>{
     console.log("server Started");
