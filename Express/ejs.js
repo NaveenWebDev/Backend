772ec770d991b953b292605ad09fc8b66397d5b9
NaveenWebDev <ns222280@gmail.com>
2023-09-22 23:00:07 -0700

    basic ejs completed

---
+++ Express/ejs.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+const port = 3000;
+
+app.listen(port, ()=>{
+    console.log(`app is listening of port no. ${port}`);
+})
+
+app.set("view enginne", "ejs"); 
+
+app.get("/", (req, res)=>{
+    res.render("home.ejs");
+})
